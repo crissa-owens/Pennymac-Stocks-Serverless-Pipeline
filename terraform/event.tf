b491c20cac416e52c0e8ee283b10b1d3342552ae
Crissa Owens <crissaowens@gmail.com>
2026-03-03 17:08:35 -0500

    package and test ingestion lambda logic

---
+++ terraform/event.tf
@@ -0,0 +1,18 @@
+resource "aws_cloudwatch_event_rule" "daily" {
+  name                = "daily-stock-ingest"
+  schedule_expression = "cron(0 0 * * ? *)"  # runs every day at midnight UTC
+}
+
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule      = aws_cloudwatch_event_rule.daily.name
+  target_id = "stockLambda"
+  arn       = aws_lambda_function.stock_ingest.arn
+}
+
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.stock_ingest.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.daily.arn
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "massive_api_key" {
+  type = string
+  description = "API key for Massive stock data"
+  sensitive = true
+}
